4d8d6437c4307cc29edbdb298f6db69b1901dc15
Ivan Mikheykin <ivan.mikheykin@flant.com>
2021-01-22 14:46:41 +0300

    Linux install

---
+++ autoexec.bat
@@ -0,0 +1,5 @@
+seta r_mode "-1"
+seta r_customwidth "1280"
+seta r_customheight "720"
+seta cg_fov "115"
+
---
+++ install-linux-apt.sh
@@ -0,0 +1,279 @@
+#!/bin/bash
+
+
+
+#
+# 	Install ioquake3 on deb based system (based on macOs script)
+#
+#       Linux changes from: @diafour
+# 	
+# 	Developer: Diego Ulloa [ @diegoullao ]
+# 	
+# 	Email: diegoulloao@icloud.com
+# 	Website: http://www.diegoulloa.cl/
+# 	
+# 	@version: 3
+# 
+
+
+
+set -e		# exit on error
+
+
+
+# VARIABLES
+declare -r BRANCH="master"
+
+
+
+# URL's: extras - dependencies
+
+# Repository
+declare -r ioquake3="https://github.com/diegoulloao/ioquake3-mac-install/raw/$BRANCH/dependencies/ioquake3-1.36-x64.zip"
+declare -r pak0="https://github.com/diegoulloao/ioquake3-mac-install/raw/$BRANCH/dependencies/baseq3/pak0/pak0.@"
+declare -r pak="https://github.com/diegoulloao/ioquake3-mac-install/raw/$BRANCH/dependencies/baseq3/pak@.pk3"
+declare -r cd_key="https://github.com/diegoulloao/ioquake3-mac-install/raw/$BRANCH/dependencies/baseq3/q3key"
+
+declare -r xpr="https://github.com/diegoulloao/ioquake3-mac-install/raw/$BRANCH/extras/extra-pack-resolution.pk3"
+declare -r q3_ls="https://github.com/diegoulloao/ioquake3-mac-install/raw/$BRANCH/extras/quake3-live-sounds.pk3"
+declare -r hd_weapons="https://github.com/diegoulloao/ioquake3-mac-install/raw/$BRANCH/extras/hd-weapons.pk3"
+
+declare -r autoexec_cfg="https://github.com/diafour/ioquake3-mac-install/raw/$BRANCH/autoexec.cfg"
+
+# External
+# declare -r hi_res="http://ioquake3.org/files/xcsv_hires.zip"
+declare -r hi_res="https://download.d-l.fr/apache_listing//jeux/quake3/common/xcsv_hires.zip"
+declare -r mappack="https://cdn.playmorepromode.com/files/cpma-mappack-full.zip"
+declare -r cpma="https://cdn.playmorepromode.com/files/cpma/cpma-1.52-nomaps.zip"
+
+
+
+# Installation begins
+cd
+
+
+
+# ioquake3
+
+
+echo "\n
+++++++++++++++++++++++++++++++++++++++++++++++++++
+	Installing ioquake3 1.36 ...
+++++++++++++++++++++++++++++++++++++++++++++++++++
+\n"
+
+sudo apt-get install ioquake3
+
+echo "\n\n-> ioquake 1.36 installed. 	(1 of 9)"
+
+
+# Makes Quake3 and baseq3 dir
+cd "$HOME/.q3a/baseq3/"
+
+
+# gets pak.pk3 files
+echo "\n
+**************************************************
+	Getting pak.pk3 ...
+**************************************************
+\n"
+
+# pak0.pk3 compressed by parts
+COUNT=0
+
+while [ $COUNT -lt 5 ] ; do
+
+	if [[ $COUNT == 0 ]] ; then
+		ext="zip"
+	else
+		ext="z0$COUNT"
+	fi
+
+	[[ $COUNT > 0 ]] && echo "\n"
+	echo "\tDownloading pk0.$ext -> pak0.pk3 ...
+	\n"
+
+	curl -L ${pak0/@/$ext} > pak0.$ext
+	let COUNT+=1
+
+done
+
+echo "\n
+	Installing pak0.pk3 ...
+\n"
+
+cat pak0.z01 pak0.z02 pak0.z03 pak0.z04 pak0.zip > pak0-master.zip
+unzip -a -o pak0-master.zip || true		# or true prevents "exit on error" because set -e
+
+rm -f pak0.z01
+rm -f pak0.z02
+rm -f pak0.z03
+rm -f pak0.z04
+rm -f pak0.zip
+
+rm -f pak0-master.zip
+
+# pak[1-8].pk3
+COUNT=1
+
+while [ $COUNT -lt 9 ] ; do
+
+	echo "\n
+	Downloading and installing pak$COUNT.pk3 ...
+	\n"
+
+	curl -L ${pak/@/$COUNT} > pak$COUNT.pk3
+	let COUNT+=1
+
+done
+
+echo "\n\n-> pak.pk3 installed. 	(2 of 9)"
+
+
+
+# High Resolution Pack
+echo "\n
+**************************************************
+	Downloading High Resolution Pack ...
+**************************************************
+\n"
+
+curl -L $hi_res > xcsv_hires.zip
+
+echo "\n
+++++++++++++++++++++++++++++++++++++++++++++++++++
+	Installing High Resolution Pack ...
+++++++++++++++++++++++++++++++++++++++++++++++++++
+\n"
+
+unzip -a -o xcsv_hires.zip
+rm -f xcsv_hires.zip
+
+echo "\n\n-> high resolution pack installed. 	(3 of 9)"
+
+
+
+# Extra pack resolution
+echo "\n
+**************************************************
+	Downloading Extra Pack Resolutions ...
+**************************************************
+\n"
+
+curl -L $xpr > pak9hqq37test20181106.pk3
+
+echo "\n\n-> extra pack resolutions installed. 	(4 of 9)"
+
+
+
+# CPMA map pack
+echo "\n
+**************************************************
+	Downloading CPMA Map-Pack ...
+**************************************************
+\n"
+
+curl -L $mappack > cpma-mappack-full.zip
+
+echo "\n
+++++++++++++++++++++++++++++++++++++++++++++++++++
+	Installing CPMA Map-Pack ...
+++++++++++++++++++++++++++++++++++++++++++++++++++
+\n"
+
+unzip -a -o -d . cpma-mappack-full.zip
+rm -f cpma-mappack-full.zip
+
+echo "\n\n-> cpma map-pack installed. 	(5 of 9)"
+
+
+
+# Q3 live sounds pack
+echo "\n
+**************************************************
+	Downloading Quake3 Live Soundpack ...
+**************************************************
+\n"
+
+curl -L $q3_ls > quake3-live-soundpack.pk3
+
+echo "\n\n-> quake3 live soundpack installed. 	(6 of 9)"
+
+
+
+# CPMA MOD
+echo "\n
+**************************************************
+	Downloading CPMA Mod 1.52 ...
+**************************************************
+\n"
+
+cd ..
+curl -L $cpma > cpma.zip
+
+echo "\n
+++++++++++++++++++++++++++++++++++++++++++++++++++
+	Installing CPMA Mod 1.52 ...
+++++++++++++++++++++++++++++++++++++++++++++++++++
+\n"
+
+unzip -a -o cpma.zip
+rm -f cpma.zip
+
+echo "\n\n-> cpma mod 1.52 installed. 	(7 of 9)"
+
+
+
+# cd-key
+echo "\n
+**************************************************
+	Downloading and installing cd-key ...
+**************************************************
+\n"
+
+cd baseq3
+
+curl -L $cd_key > q3key
+
+echo "\n\n-> cd-key installed. 	(8 of 9)"
+
+
+
+# hd-weapons
+echo "\n
+**************************************************
+	Downloading and installing hd-weapons ...
+**************************************************
+\n"
+
+curl -L $hd_weapons > pakxy01Tv5.pk3
+
+echo "\n\n-> hd-weapons installed. 	(9 of 9)"
+
+# autoexec.cfg
+echo "\n
+**************************************************
+	Downloading autoexec.cfg ...
+**************************************************
+\n"
+
+curl -L $autoexec_cfg > autoexec.cfg
+
+echo "\n\n-> autoexec.cfg installed."
+
+
+
+# Done.
+echo "\n
+---------------------------------------------------
+	QUAKE 3 ARENA: SUCCESSFUL INSTALLATION.
+---------------------------------------------------
+	Now to kick asses! ->
+---------------------------------------------------
+
+-> follow on github: @diegoulloao :)
+\n"
+
+
+
+# end
